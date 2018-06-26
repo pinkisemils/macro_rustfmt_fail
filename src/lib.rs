8cc28c5d47225a01c1b0fbb447130fb90b31a724
Emīls <emils@mullvad.net>
2018-06-26 10:22:58 +0100

    Formatting failure

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-
 #[macro_export]
-macro_rules! ffi_error {
-    ($result:ident, $error:expr) => {
-        #[repr(C)]
-        #[derive(Debug)]
+macro_rules! hmmm___ffi_error {
+    ($result:ident) => {
         pub struct $result {
             success: bool,
         }
         
         impl $result {
-            pub fn into_result(self) -> Result<()> {
-                match self.success {
-                    true => Ok(()),
-                    false => Err($error),
-                }
-            }
-        }
-        
-        impl Into<Result<()>> for $result {
-            fn into(self) -> Result<()> {
-                self.into_result()
-            }
+            pub fn foo(self) {}
         }
     };
 }
